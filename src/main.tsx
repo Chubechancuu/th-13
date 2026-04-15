4e366320c920d08e480ddb526a5834e2318e0822
Chubechancuu <cavangaza@gmail.com>
2026-04-16 02:53:32 +0700

    dc

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// Tuyệt đối KHÔNG có dòng import './index.css' ở đây nhé!
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
